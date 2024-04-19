1968934ef3033eb7dd5342b0862d12cd9799f75d
dmal0 <143022794+dmal0@users.noreply.github.com>
2024-04-18 19:08:31 -0600

    Upload part 2 files from LMVM

--- part2/print_self.c
+++ part2/print_self.c
@@ -2,6 +2,9 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
+/* Variable to hold the names of different process states */
+char *process_state[65] = { [0 ... 64] = "UNKNOWN_STATE"};
+
 /* Print process info for self and parents */
 int print_self_and_parents(void){
     /* Use the Task struct task_struct */ 
@@ -9,24 +12,34 @@ int print_self_and_parents(void){
        running process' task struct */
     struct task_struct *task = current;
 
+    /* Map numeric state with known process string 
+       states */
+    process_state[0] = "TASK_RUNNING";
+    process_state[1] = "TASK_INTERRUPTIBLE";
+    process_state[2] = "TASK_UNINTERRUPTIBLE";
+    process_state[4] = "TASK_STOPPED";
+    process_state[8] = "TASK_TRACED";
+    process_state[16] = "EXIT_ZOMBIE";
+    process_state[32] = "EXIT_DEAD";
+    process_state[64] = "TASK_NONINTERACTIVE";
+
     /* Print info for the current process */
-    /* Since this is used twice I think it could be its own 
-       function but maybe that can be for later */
     printk("Current process information:\n");
     printk("Name: %s\n", task->comm); /* comm has process name */
     printk("ID: %d\n", task->pid);
-    printk("State: %ld\n", task->state);
+    printk("State: %s\n", process_state[ task->state ]);
 
     /* Traverse through parents */
     /* Only continue until reaching init whose PID == 1*/
     while (task->pid != 1){
+        struct task_struct *prevTask = task;
         task = task->parent; /* Traverse through parents */
 
         /* Print process info */
-        printk("Current process information:\n");
+        printk("Current process information (parent of %s):\n", prevTask->comm);
         printk("Name: %s\n", task->comm); /* comm has process name */
         printk("ID: %d\n", task->pid);
-        printk("State: %ld\n", task->state);
+        printk("State: %s\n", process_state[ task->state ]);
     }
 
     return 0;
@@ -34,7 +47,7 @@ int print_self_and_parents(void){
 
 int pself_init_module(void){
     printk(KERN_INFO "Initializing print_self kernel module...\n");
-    print_self_and_parents()
+    print_self_and_parents();
     return 0;
 }
 
